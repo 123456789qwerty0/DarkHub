2debb6522415a8b5366fa32fbfa568734f9c7260
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-07-28 11:04:43 -0400

    Update Credits.lua

--- gui/Chams/ViewTracer/Credits.lua
+++ gui/Chams/ViewTracer/Credits.lua
@@ -2,7 +2,7 @@ local NotificationHolder = loadstring(game:HttpGet("https://raw.githubuserconten
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/Notifications/NotificationLIB/Client.lua"))()
 
 Notification:Notify(
-    {Title = "Script Credits", Description = "This script was made by Blissful#4992!"},
+    {Title = "Script Credits", Description = "This script was made by Blissful#4992! [https://github.com/Blissful4992] "},
     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "option"},
     {Image = "http://www.roblox.com/asset/?id=18684458505", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
 )
