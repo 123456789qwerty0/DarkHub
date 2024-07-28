41e9a8ed06a21d5033d24ee3f514d8f3c9e3c8ef
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-07-28 17:49:12 -0400

    Create Loader.lua

---
+++ gui/Chams/UESP/Loader.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/UESP/Loaded.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/UESP/Credits.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/UESP/UESP.lua",true))()
