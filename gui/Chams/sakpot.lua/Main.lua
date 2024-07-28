82ddfa325fcf08eef27899fa8a8c9eb3a6288df8
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-07-28 17:18:23 -0400

    Create Main.lua

---
+++ gui/Chams/sakpot.lua/Main.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/sakpot.lua/Loaded.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/sakpot.lua/info.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/sakpot.lua/Credits.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/sakpot.lua/sakpot.lua",true))()
