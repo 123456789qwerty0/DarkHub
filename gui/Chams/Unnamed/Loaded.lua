b761845ddbf56c19a1858f04a9a2a440e0958259
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-07-28 09:34:15 -0400

    Create Loaded.lua

---
+++ gui/Chams/Unnamed/Loaded.lua
@@ -0,0 +1,8 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/Notifications/NotificationLIB/Module.lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/Notifications/NotificationLIB/Client.lua"))()
+
+Notification:Notify(
+    {Title = "Loaded!", Description = "Unnamed ESP Loaded!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "option"},
+    {Image = "http://www.roblox.com/asset/?id=18684458505", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
+)
