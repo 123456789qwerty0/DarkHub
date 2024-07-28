a729a1971c873b38fc1693d2b7a7e9ca946a4b37
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-07-28 11:13:03 -0400

    Create Main.lua

---
+++ gui/Chams/Skeleton/Main.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/Skeleton/Loaded.lua",true))()
+wait(2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/Skeleton/Credits.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/Skeleton/SkeletonChams.lua",true))()
