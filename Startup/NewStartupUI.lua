fa80ce257b7135c336cd235f3d1e4a45c5592334
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-07-28 12:38:06 -0400

    Update NewStartupUI.lua

--- Startup/NewStartupUI.lua
+++ Startup/NewStartupUI.lua
@@ -1,76 +1,33 @@
 --discord ui
 local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/UiLibraries/DiscordUI/Lib.lua")()
 
-local win = DiscordLib:Window("discord library")
+local win = DiscordLib:Window("Darkhub")
 
-local serv = win:Server("DarkHub", "")
+local serv = win:Server("Startup Menu", "")
 
-local btns = serv:Channel("Main")
+local btns = serv:Channel("main")
 
 btns:Button("Load DarkHub", function()
 DiscordLib:Notification("Notification", "Killed everyone!", "Okay!")
 end)
-
-btns:Seperator()
-
-btns:Button("Get max level", function()
+  
+local github = serv:Channel("github")
+github:Button("Copy Github", function()
 setclipboard("https://github.com/123456789qwerty0/DarkHub/tree/main")
 end)
 
-local tgls = serv:Channel("Toggles")
-
-tgls:Toggle("Auto-Farm",false, function(bool)
-print(bool)
-end)
-
-local sldrs = serv:Channel("Sliders")
-
-local sldr = sldrs:Slider("Slide me!", 0, 1000, 400, function(t)
-print(t)
-end)
-
-sldrs:Button("Change to 50", function()
-sldr:Change(50)
-end)
-
-local drops = serv:Channel("Dropdowns")
+local lbls = serv:Channel("Supported Games")
+lbls:Seperator
+lbls:Label("List of supported games:")
+lbls:Seperator
+lbls:Label("MM2")
+lbls:Label("Influencer Meeting Place")
+lbls:Label("Doors")
+lbls:Label("KAT")
+lbls:Label("Clip It")
+lbls:Label("Color Or Die")
+lbls:Label("Rainbow Friends")
+lbls:Label("Rainbow Friends Chapter 2")
+lbls:Label("Piggy")
 
 
-local drop = drops:Dropdown("Pick me!",{"Option 1","Option 2","Option 3","Option 4","Option 5"}, function(bool)
-print(bool)
-end)
-
-drops:Button("Clear", function()
-drop:Clear()
-end)
-
-drops:Button("Add option", function()
-drop:Add("Option")
-end)
-
-local clrs = serv:Channel("Colorpickers")
-
-clrs:Colorpicker("ESP Color", Color3.fromRGB(255,1,1), function(t)
-print(t)
-end)
-
-local textbs = serv:Channel("Textboxes")
-
-textbs:Textbox("Gun power", "Type here!", true, function(t)
-print(t)
-end)
-
-local lbls = serv:Channel("Labels")
-
-lbls:Label("This is just a label.")
-
-local bnds = serv:Channel("Binds")
-
-bnds:Bind("Kill bind", Enum.KeyCode.RightShift, function()
-print("Killed everyone!")
-end)
-
-serv:Channel("by dawid#7205")
-
-
-win:Server("Main", "http://www.roblox.com/asset/?id=6031075938")
