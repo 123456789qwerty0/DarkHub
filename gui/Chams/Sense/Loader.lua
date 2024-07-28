9a6023ad5db116ba2bb469bab7997d089d634084
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-07-28 11:22:12 -0400

    Create Loader.lua

---
+++ gui/Chams/Sense/Loader.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/Sense/Loaded.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/Sense/SenseConfig.lua",true))()
