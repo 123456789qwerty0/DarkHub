6b91ad22676e3d4395a557e9fdf0770f401d471b
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-07-28 17:15:47 -0400

    Create Credits.lua

---
+++ gui/Chams/sakpot.lua/Credits.lua
@@ -0,0 +1,8 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/Notifications/NotificationLIB/Module.lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/Notifications/NotificationLIB/Client.lua"))()
+
+Notification:Notify(
+    {Title = "Script Credits", Description = "Script made by lucasfin000 [github.com/lucasfin000]"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "option"},
+    {Image = "http://www.roblox.com/asset/?id=18684458505", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
+)
