555cc2c3e800a43d82748b2fa227b171742000e9
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-07-28 09:12:34 -0400

    Create Main.lua

---
+++ gui/Chams/ViewTracer/Main.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/ViewTracer/ScriptLoaded.lua",true))()
+wait(2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/ViewTracer/Credits.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/ViewTracer/ViewTracer.lua",true))()
