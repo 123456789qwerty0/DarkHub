ef5f2a66e21e43d1949f793ab16afaa2eaa3b1ba
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-07-28 09:38:51 -0400

    Create Main.lua

---
+++ gui/Chams/Unnamed/Main.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/Unnamed/Loaded.lua",true))()
+wait(2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/Unnamed/Credits.lua",true))()
+pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/Unnamed/Unnanmed.lua'))() end)
