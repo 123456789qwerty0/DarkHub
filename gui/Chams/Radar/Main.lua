a22bd38a8dbf140ef88209cabe839f153f23a170
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-07-29 07:47:43 -0400

    Create Main.lua

---
+++ gui/Chams/Radar/Main.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/Radar/Loaded.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/Radar/Credits.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/gui/Chams/Radar/Radar.lua",true))()
