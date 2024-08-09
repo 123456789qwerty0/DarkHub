0d4f2750d0aaeeba0fb3a5dae24f5f6db8b8e8a8
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-08-09 11:12:20 -0400

    Update and rename crucifix.lua to Crucifix.lua

---
+++ Scripts/Games/Doors/Crucifix/Crucifix.lua
@@ -0,0 +1,15 @@
+--[[
+
+ ________  ________  ________  ___  __    ___  ___  ___  ___  ________     
+|\   ___ \|\   __  \|\   __  \|\  \|\  \ |\  \|\  \|\  \|\  \|\   __  \    
+\ \  \_|\ \ \  \|\  \ \  \|\  \ \  \/  /|\ \  \\\  \ \  \\\  \ \  \|\ /_   
+ \ \  \ \\ \ \   __  \ \   _  _\ \   ___  \ \   __  \ \  \\\  \ \   __  \  
+  \ \  \_\\ \ \  \ \  \ \  \\  \\ \  \\ \  \ \  \ \  \ \  \\\  \ \  \|\  \ 
+   \ \_______\ \__\ \__\ \__\\ _\\ \__\\ \__\ \__\ \__\ \_______\ \_______\
+    \|_______|\|__|\|__|\|__|\|__|\|__| \|__|\|__|\|__|\|_______|\|_______|
+    
+                https://github.com/123456789qwerty0/DarkHub/tree/main
+]]--
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/Scripts/Games/Doors/Functions/functions.lua"))()
+LoadCustomInstance("https://raw.githubusercontent.com/123456789qwerty0/DarkHub/main/Scripts/Games/Doors/Crucifix/Crucifix.rbxm").Parent = game:GetService("Players").LocalPlayer.Backpack
